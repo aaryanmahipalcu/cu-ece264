47ae21e1500d5c80ef954edefee7ad2a897f9309
aaryanmahipalcu <91095344+aaryanmahipalcu@users.noreply.github.com>
2022-03-27 20:32:25 -0400

    Add files via upload

---
+++ ece264_1.c
@@ -0,0 +1,64 @@
+//Aaryan Mahipal
+#include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+class Foobar{    //This is my Base Class
+    public: 
+
+};
+
+class Foo: public Foobar{
+
+};
+
+class Bar: public Foobar{
+
+};
+
+void userInput(){
+    //This function reads each line of the input file
+}
+
+void userOutput(){
+    //This function loops through the vector and writes to the output file
+}
+
+//Declaring Functions
+std::string get_input_filename()
+std::string get_output_filename()
+std::string get_file_contents(std::string const& infile)
+int main(){
+
+}
+
+//Implementing Functions
+std::string get_input_filename(){
+    std::string filename;
+    cout << "Enter Input filename\n";
+    cout << "Filename: ";
+    cin >> filename;
+    return filename;
+}
+
+std::string get_output_filename(){
+    std::string filename;
+    cout << "Enter Output filename\n";
+    cout << "Filename: ";
+    cin >> filename;
+    return filename;
+}
+
+std::string get_file_contents(std::string const& infile){
+    std::ifstream inf(infile);
+    std::string contents;
+    int c;
+    while ((c = inf.get()) != EOF){
+        contents += c;
+    }
+    return contents;
+}
+
+//use vectors to store the word and its location
