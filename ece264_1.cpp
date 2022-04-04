cc27d3e3a4a17d16b2b02296b84136e7f41d0923
aaryanmahipalcu <aaryan.mahipal@cooper.edu>
2022-04-04 16:35:31 -0400

    n

--- ece264_1.cpp
+++ ece264_1.cpp
@@ -13,42 +13,44 @@ string ifilename;
 string ofilename; 
 
 class Foobar{    //This is my Base Class
-    public: 
+private:
+    //name and position
+    string name;
+    int position;
+
+protected: 
+    //getter to get position
+    inline int getPosition(){
+        return position;
+    }
+    
+public: 
     //constructor
     Foobar(string n, int p){
         name = n;
         position = p;
         }
 
-    void setPosition(p){
+    inline void setPosition(int p){
         position = p;
     }
     
     //Getters
-    virtual int getStrength(){
+    inline virtual int getStrength(){
         return position;
     }
-    string Foobar::getName(){
+    inline string getName(){
         return name;
     }
     //get the strength
     //get the name
     //memeber function to set position in line
     string type;
-    private:
-    //name and position
-    string name;
-    int position;
-    protected: 
-    //getter to get position
-    int getPosition(){
-        return position;
-    }
 };
 
 class Foo: public Foobar{
     public:
-    virtual int getStrength(){
+    inline virtual int getStrength(){
         return position*3;
     }
     
@@ -56,7 +58,7 @@ class Foo: public Foobar{
 
 class Bar: public Foobar{
     public:
-    virtual int position+15;
+    inline virtual int (position)+15;
 };
 
 void getFiles(){
@@ -68,54 +70,57 @@ void getFiles(){
     cout << "Your Output Filename is: " << ofilename;
 }
 
-void userInput(string ifilename, string type, string name){
+void userInput(vector<Foobar *> &vec, string ifilename){
+    string name, type;
+    
     //This function reads each line of the input file
     ifstream my_input_file(ifilename);  
     string line; 
     my_input_file.open(ifilename);
+    int pos = 0;
     while(getline(my_input_file, line)){
         stringstream ss(line);
         ss >> type >> name;
         if(type == "foobar"){
-            Foobar*p = new Foobar(name, 0);
-            mainvector.push_back(p);
+            Foobar*p = new Foobar(name, pos);
+            vec.push_back(p);
         }
         else if(type == "foo"){
-            Foobar*p = new Foo(name, 0);
-            mainvector.push_back(p);
+            Foobar*p = new Foo(name, pos);
+            vec.push_back(p);
         }
-        else(type == "bar"){
-            Foobar*p = new Bar(name, 0);
-            mainvector.push_back(p);
+        else if(type == "bar"){
+            Foobar*p = new Bar(name, pos);
+            vec.push_back(p);
         }
+        ++pos;
     }
     my_input_file.close();
 }
 
-void update_strength(){
+/*void update_strength(){
     int n;
     for(int i=0; i<=n; ++i){
         Foobar *newp = new Foobar(i);
         mainvector.push_back(newp);
     }
-}
+}*/
 
-void userOutput(string ofilename){
+void userOutput(vector<Foobar *> &vec, string ofilename){
     //This function loops through the vector and writes to the output file
     ofstream my_output_file(ofilename);
     my_output_file.open(ofilename);
-    int k;
-    for(int j=0; j<=k; ++j){
-        cout << name << " " << newp;
+    for(auto foobar : vec){
+        cout << foobar->getName() << " " << foobar->getStrength();
     }
 }
 
 int main(){
     getFiles();
     vector<Foobar *> mainvector;
-    userInput(ifilename," "," ");
-    update_strength();
-    userOutput(ofilename);
+    userInput(mainvector, ifilename);
+    //update_strength();
+    userOutput(mainvector, ofilename);
 }
 
 //use vectors to store the word and its location
