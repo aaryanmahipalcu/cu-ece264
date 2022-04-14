b82dfbf9b1771b136b14fc04768b5827a5e1cf3f
aaryanmahipalcu <aaryan.mahipal@cooper.edu>
2022-04-14 14:23:33 -0400

    Update ece264_2.cpp

--- Program 2/ece264_2.cpp
+++ Program 2/ece264_2.cpp
@@ -7,6 +7,19 @@
 
 using namespace std;
 
+//Global Variables
+string ifilename;
+string ofilename;
+
+void getFiles(){
+    cout << "Enter input file name: ";
+    cin >> ifilename;
+    cout << "Your Input Filename is: " << ifilename;
+    cout << "Enter output file name: ";
+    cin >> ofilename;
+    cout << "Your Output Filename is: " << ofilename;
+}
+
 int main(){
-    
+    getFiles();
 }
